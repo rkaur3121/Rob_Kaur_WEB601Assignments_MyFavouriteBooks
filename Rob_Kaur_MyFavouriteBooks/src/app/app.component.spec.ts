9e6f5c69ffafac956ef63ef491538179cd7e9b76
rkaur3121 <115298724+rkaur3121@users.noreply.github.com>
2023-05-30 16:57:27 -0400

    edit files and add images

--- Rob_Kaur_MyFavouriteBooks/src/app/app.component.spec.ts
+++ Rob_Kaur_MyFavouriteBooks/src/app/app.component.spec.ts
@@ -12,16 +12,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Rob_Kaur_MyFavouriteBooks'`, () => {
+  it(`should have as title 'Rob_Kaur_FavouriteBooks'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Rob_Kaur_MyFavouriteBooks');
+    expect(app.title).toEqual('Rob_Kaur_FavouriteBooks');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('Rob_Kaur_MyFavouriteBooks app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Rob_Kaur_FavouriteBooks app is running!');
   });
 });
