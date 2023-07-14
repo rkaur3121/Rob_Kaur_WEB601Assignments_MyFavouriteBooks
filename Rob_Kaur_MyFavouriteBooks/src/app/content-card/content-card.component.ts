7289d958279ff620d15218134bb308399892209d
rkaur3121 <115298724+rkaur3121@users.noreply.github.com>
2023-07-13 21:07:37 -0400

    edit this files

--- Rob_Kaur_MyFavouriteBooks/src/app/content-card/content-card.component.ts
+++ Rob_Kaur_MyFavouriteBooks/src/app/content-card/content-card.component.ts
@@ -9,31 +9,35 @@ export class ContentCardComponent {
 
   constructor() {
     this.contentList.add({
-      id: 1,
-      title: 'Naruto',
-      description: 'Naruto Des',
-      creator: 'Temp',
-      imgURL: 'https://www.google.com/search?sxsrf=AB5stBiyi2sXBada-ciYJyp8lqSJD-hk0A:1688689828923&q=Naruto&tbm=isch&sa=X&ved=2ahUKEwjk243rq_v_AhXgkokEHWrxDNAQ0pQJegQICxAB&biw=1536&bih=726&dpr=1.25#imgrc=kWVsVVfqP_aULM',
-      type: 'Action',
-      tags: ['Fight', 'Adventure']
+      
+        id: 1,
+        title: 'To Kill a Mockingbird',
+        description: 'A Classic coming-of-age story that explores themes of racial injustice and morality',
+        creator: 'Harper lee',
+        imgURL: 'assets/image/mockingbird.jpg',
+        type: 'Fiction',
+        tags: ['classic,2015'],
+        highlight: false
     });
     this.contentList.add({
       id: 2,
-      title: 'DeathNote',
-      description: 'DeathNote Des',
-      creator: 'Temp2',
-      imgURL: 'https://www.google.com/search?q=Deathnote&tbm=isch&ved=2ahUKEwij6PX5q_v_AhVkFmIAHTC8AT8Q2-cCegQIABAA&oq=Deathnote&gs_lcp=CgNpbWcQAzIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQ6BAgjECc6CggAEIoFELEDEEM6BwgAEIoFEEM6CAgAEIAEELEDUJwKWOMdYOoeaABwAHgBgAGQAogBnwiSAQU4LjEuMZgBAKABAaoBC2d3cy13aXotaW1nwAEB&sclient=img&ei=w1ynZOP6O-SsiLMPsPiG-AM&bih=726&biw=1536#imgrc=feb8bFZ1PTfryM',
-      type: 'Suspence',
-      tags: ['Fight', 'Thriller', 'Horror']
+        title: 'The Great Gatsby',
+        description: 'Set in the Roaring Twenties, this novel depicts the glamorous and decadent lives of wealthy individuals and the emptiness that lies beneath the surface',
+        creator: 'F.Scott Fitzgerald',
+        imgURL: 'assets/image/great.jpg',
+        type: 'Fiction',
+        tags: ['science fiction,2013'],
+        highlight: true
     });
     this.contentList.add({
       id: 3,
-      title: 'Black Clover',
-      description: 'Black Clover Des',
-      creator: 'temp3',
-      imgURL: 'https://www.google.com/search?q=Black+clover&tbm=isch&ved=2ahUKEwjz5JaQrPv_AhVmN1kFHeSoC0gQ2-cCegQIABAA&oq=Black+clover&gs_lcp=CgNpbWcQAzIICAAQgAQQsQMyCAgAEIAEELEDMggIABCABBCxAzIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQ6BAgjECc6BwgAEIoFEEM6CggAEIoFELEDEEM6BAgAEANQ0A1Y4Cxg1C1oAHAAeAKAAYgLiAHiKZIBDTcuMC4xLjEuNi0xLjOYAQCgAQGqAQtnd3Mtd2l6LWltZ8ABAQ&sclient=img&ei=8lynZPOjKObu5NoP5NGuwAQ&bih=726&biw=1536#imgrc=AqtlJndHRIFVSM',
-      type: 'Action',
-      tags: ['Action', 'Fight']
+        title: 'Pride & predudice',
+        description: 'A classic romance novel featuring the spirited Elizabeth bennet and her tumultuous relationship with the enigmatic Mr-Darcy in 19th-century England',
+        creator: 'Jane Austen',
+        imgURL: 'assets/image/image4.jpg',
+        type: 'Fiction',
+        tags: ['classic,2010'],
+        highlight: true
     });
   }
 }
--- Rob_Kaur_MyFavouriteBooks/src/app/content-list/content-list.component.ts
+++ Rob_Kaur_MyFavouriteBooks/src/app/content-list/content-list.component.ts
@@ -1,95 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TravelDestinationService } from '../services/travel-destination.service';
+import { contentListArray } from '../helper_files/contentDB';
 
 @Component({
   selector: 'app-content-list',
   templateUrl: './content-list.component.html',
   styleUrls: ['./content-list.component.scss']
 })
-export class ContentListComponent {
+export class ContentListComponent implements OnInit {
+  
+  contentListArray:any[];
 
+  constructor(private traveldestinationService:TravelDestinationService){
+    this.contentListArray = contentListArray;
+  }
+  ngOnInit(): void {
+    this.traveldestinationService.getContentArray().subscribe((data: any[])=>{
+      this.contentListArray=data;
+    });
+  }
   highlightedItemIndex: number = -1;
   searchTitle: string = '';
   searchResult: string = '';
   isContentFound: boolean = false;
-  contentListArray: any[] = [];
- 
 
-  constructor() {
-    // Initialize content list if needed
-     this.contentListArray = [
-      {
-        id: 1,
-        title: 'To Kill a Mockingbird',
-        description: 'A Classic coming-of-age story that explores themes of racial injustice and morality',
-        creator: 'Harper lee',
-        imgURL: 'assets/image/mockingbird.jpg',
-        type: 'Fiction',
-        tags: ['classic,2015'],
-        highlight: false
-      },
-      {
-        id: 2,
-        title: 'The Great Gatsby',
-        description: 'Set in the Roaring Twenties, this novel depicts the glamorous and decadent lives of wealthy individuals and the emptiness that lies beneath the surface',
-        creator: 'F.Scott Fitzgerald',
-        imgURL: 'assets/image/great.jpg',
-        type: 'Fiction',
-        tags: ['science fiction,2013'],
-        highlight: true
-      },
-      {
-        id: 3,
-        title: 'Pride & predudice',
-        description: 'A classic romance novel featuring the spirited Elizabeth bennet and her tumultuous relationship with the enigmatic Mr-Darcy in 19th-century England',
-        creator: 'Jane Austen',
-        imgURL: 'assets/image/image4.jpg',
-        type: 'Fiction',
-        tags: ['classic,2010'],
-        highlight: true
-      },
-      {
-        id: 4,
-        title: 'The Lord of the Rings',
-        description: 'An epic fantasy trilogy that follows a diverse group of characters on a perilous quest to destroy a powerful ring and save Middle-earth from darkness.',
-        creator: 'J.R.R. Tolkien',
-        imgURL: 'assets/image/lord.jpg',
-        type: 'Fiction',
-        tags: ['contemporary,2013'],
-        highlight: false
-      },
-      {
-        id: 5,
-        title: 'The Alchemist',
-        description: 'A philosophical novel about a young Andalusian shepherd boy who embarks on a journey to discover his personal legend and the true meaning of life.',
-        creator: 'Paulo Coelho',
-        imgURL: 'assets/image/the.jpg',
-        type: 'Fiction',
-        tags: ['american literature,2010'],
-        highlight: true
-      },
-      {
-        id: 6,
-        title: '1984',
-        description: 'A dystopian novel set in a totalitarian society ',
-        creator: 'George Orwell',
-        imgURL: 'assets/image/img.jpg',
-        type: 'Fiction',
-        tags: ['science,fiction'],
-        highlight:false
-      },
-      {
-        id: 7,
-        title: 'The Catcher in the Rye',
-        description: 'A coming-of-age novel',
-        creator: 'J.D. Salinger',
-        imgURL: 'assets/image/catcher1.jpg',
-        type: 'Action',
-        tags: [' Animation, 2012'],
-        highlight: false
-      }
-    ];
-  }
-
+  
 
   displayContent(contentItem: any) {
     console.log('Content ID:', contentItem.id);
@@ -101,14 +36,5 @@ export class ContentListComponent {
     this.isContentFound = !!foundItem;
     this.searchResult = this.isContentFound ? 'Content item found!' : 'Content item not found!';
  
-  }
-
- 
-
-  receiveNewContent(newContent: any): void {
-    // Add the new content item to the content list
-    console.log("newContent",newContent);
-    this.contentListArray.push(newContent);
-    console.log('contentListArray',this.contentListArray);
   }
 }
