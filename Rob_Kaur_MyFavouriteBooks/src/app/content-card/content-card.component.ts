c0cfcca59d50730dcb361d3457b47b4d8e17df64
rkaur3121 <115298724+rkaur3121@users.noreply.github.com>
2023-07-23 12:20:36 -0400

    Assignment-7

--- Rob_Kaur_MyFavouriteBooks/src/app/content-card/content-card.component.ts
+++ Rob_Kaur_MyFavouriteBooks/src/app/content-card/content-card.component.ts
@@ -8,36 +8,6 @@ export class ContentCardComponent {
   contentList: any = [];
 
   constructor() {
-    this.contentList.add({
-      
-        id: 1,
-        title: 'To Kill a Mockingbird',
-        description: 'A Classic coming-of-age story that explores themes of racial injustice and morality',
-        creator: 'Harper lee',
-        imgURL: 'assets/image/mockingbird.jpg',
-        type: 'Fiction',
-        tags: ['classic,2015'],
-        highlight: false
-    });
-    this.contentList.add({
-      id: 2,
-        title: 'The Great Gatsby',
-        description: 'Set in the Roaring Twenties, this novel depicts the glamorous and decadent lives of wealthy individuals and the emptiness that lies beneath the surface',
-        creator: 'F.Scott Fitzgerald',
-        imgURL: 'assets/image/great.jpg',
-        type: 'Fiction',
-        tags: ['science fiction,2013'],
-        highlight: true
-    });
-    this.contentList.add({
-      id: 3,
-        title: 'Pride & predudice',
-        description: 'A classic romance novel featuring the spirited Elizabeth bennet and her tumultuous relationship with the enigmatic Mr-Darcy in 19th-century England',
-        creator: 'Jane Austen',
-        imgURL: 'assets/image/image4.jpg',
-        type: 'Fiction',
-        tags: ['classic,2010'],
-        highlight: true
-    });
+    this.contentList.add();
   }
 }
